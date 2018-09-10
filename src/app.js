de41163fb823045fec0c4836b87d9dc4697769fb
HarryCZ <jakub.voboril@gmail.com>
2018-09-10 22:50:05 +0200

    completed

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('index', {
     title: 'Account Summary',
